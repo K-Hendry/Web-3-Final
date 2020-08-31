9a70c69e3addadd34aeb1c5310dabd5556b26ab8
aaronwmonro <63673145+aaronwmonro@users.noreply.github.com>
2020-08-30 20:03:09 -0400

    progress bar

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -6,11 +6,13 @@
 // @codekit-prepend "scrollTrigger/paragraphST.js";
 // @codekit-prepend "scrollTrigger/pullQuoteST.js";
 // @codekit-prepend "scrollTrigger/headerST.js";
+// @codekit-prepend "scrollTrigger/progress-bar.js";
 
 // @codekit-prepend "employment/counter.js"; 
 // @codekit-prepend "impact/impact-pin.js"; 
 // @codekit-prepend "impact/impact-para.js"; 
 // @codekit-prepend "gallery/gallerySwipe.js"; 
+
 
 // @codekit-prepend "main-nav/nav-global.js";
 // @codekit-prepend "main-nav/main-nav-click.js";
